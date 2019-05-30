23f119facd3f01c71f651f70c937b8118b2de39b
Charles Feval <chfeval@microsoft.com>
2019-05-30 09:53:28 -0700

    Adding purpose

---
+++ AddPurpose/run.ps1
@@ -0,0 +1,43 @@
+param($Timer)
+$date=Get-Date
+$defaultTag=$env:DEFAULT_TAG
+
+Write-Host "Trying to auto-determine purpose at $date"
+Select-AzSubscription -Subscription $env:SUBSCRIPTION
+$untaggedResources = Get-AzResourceGroup | Where-Object { ! $_.Tags.purpose }
+Foreach ($resourceGroup in $untaggedResources) {
+    $tagToApply = $defaultTag
+    $groupName = $resourceGroup.ResourceGroupName
+    if ($resourceGroup.ResourceGroupName -match 'MC_*') {
+        $mainResourceGroupName = $groupName.Substring(3)
+        $indexOfBase = $mainResourceGroupName.IndexOf("_")
+        if ($indexOfBase -ge 0) {
+            $mainResourceGroupName = $mainResourceGroupName.Substring(0, $indexOfBase)
+        }
+        $mainResourceGroup = Get-AzResourceGroup -Name $mainResourceGroupName
+        if ($mainResourceGroup) {
+            $tagToApply = $mainResourceGroup.Tags.purpose
+        } else {
+            Write-Host "Ignoring $groupName - no main resource group found"
+            continue
+        }
+    } elseif ($groupName -match 'prod*' `
+        -or $groupName -match 'keep*' `
+        -or $groupName -match 'Vsts*' `
+        -or $groupName -match 'NetworkWatcherRG' `
+        -or $groupName -match 'cloud-shell-storage*') {
+        $tagToApply = "production"
+    } elseif ($groupName -match 'exp*' `
+        -or $groupName -match 'garbage*') {
+        $tagToApply = "experiment"
+    }
+    Write-Host "Apply tag $tagToApply to group $groupName"
+    $tags = $resourceGroup.Tags
+    if (! $tags) {
+        $tags = @{ purpose=$tagToApply }
+    } else {
+        $tags.Add("purpose", $tagToApply)
+    }
+    Set-AzResourceGroup -Tag $tags -Name $resourceGroup.ResourceGroupName
+}
+Write-Host "Done adding tags"
