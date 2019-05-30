7fb788d604a94da19e186106d8997e61fba695c5
Charles Feval <chfeval@microsoft.com>
2019-05-30 10:31:23 -0700

    Fixing provisioning to remove app registration

--- provision.sh
+++ provision.sh
@@ -28,11 +28,12 @@ done
 RANDOMBASE="`random 5`"
 SUBSCRIPTIONID="`az account show --query id -o tsv`"
 SUBSCRIPTION_RESOURCE_ID="/subscriptions/$SUBSCRIPTIONID"
+NAME="`basename "$PWD"`"
 
 if [ ! -f env.sh ]; then
     echo "#!/bin/bash
 
-NAME='`basename "$PWD"`'
+NAME='$NAME'
 LOCATION=\"westus2\"
 RANDOMBASE=\"$RANDOMBASE\"
 RANDOMBASE16CHAR=\"`random 16`\"
@@ -56,13 +57,10 @@ usage() {
 
 
 echo "This will provision the following resources: "
-echo "AppRegistration (default)"
 echo "ResourceGroup (default)"
 echo "StorageAccount (default)"
 echo "FunctionApp (default)"
 
-DEFAULT_APPLICATION_IDENTIFIER_URI="https://$NAME-$RANDOMBASE.azurewebsites.net/"
-DEFAULT_APPLICATION_PASSWORD="@$RANDOMBASE16CHAR!@123001"
 DEFAULT_RESOURCE_GROUP="$NAME"
 DEFAULT_RESOURCE_GROUP_RESOURCE_ID="$SUBSCRIPTION_RESOURCE_ID/resourceGroups/$DEFAULT_RESOURCE_GROUP"
 DEFAULT_STORAGE_ACCOUNT="`echo "$STORAGEBASENAME" | sed -e 's/-//g' | sed -E 's/^(.*)$/\L\1/g' | head -c 20`def"
@@ -70,15 +68,6 @@ DEFAULT_STORAGE_ACCOUNT_RESOURCE_ID="$DEFAULT_RESOURCE_GROUP_RESOURCE_ID/provide
 DEFAULT_FUNCTIONAPP="$NAME-$RANDOMBASE"
 DEFAULT_FUNCTIONAPP_HOSTNAME="https://$DEFAULT_FUNCTIONAPP.azurewebsites.net"
 
-echo "Creating App Registration $DEFAULT_APPLICATION_IDENTIFIER_URI"
-echo "[{
-    'resourceAppId': '00000002-0000-0000-c000-000000000000',
-    'resourceAccess': [ {
-        'id': '311a71cc-e848-46a1-bdf8-97ff7156d8e6',
-        'type': 'Scope'
-        } ]
-    }]" > app-registration-manifest.tmp.json
-DEFAULT_APPLICATION_ID=`az ad app create --identifier-uris $DEFAULT_APPLICATION_IDENTIFIER_URI --available-to-other-tenants false --reply-urls $DEFAULT_FUNCTIONAPP_HOSTNAME/.auth/login/aad/callback --display-name DEFAULT_APPLICATION_IDENTIFIER_URI --password "$DEFAULT_APPLICATION_PASSWORD" --required-resource-access app-registration-manifest.tmp.json --query "appId" -o tsv`
 echo "Creating resource group $DEFAULT_RESOURCE_GROUP"
 az group create --name $DEFAULT_RESOURCE_GROUP --location $LOCATION --query "properties.provisioningState" -o tsv
 echo "Creating storage account $DEFAULT_STORAGE_ACCOUNT"
@@ -89,29 +78,22 @@ DEFAULT_STORAGE_ACCOUNT_CONNECTION_STRING=`az storage account show-connection-st
 
 echo "Creating functionapp $DEFAULT_FUNCTIONAPP"
 az functionapp create -g $DEFAULT_RESOURCE_GROUP --consumption-plan-location $LOCATION --name $DEFAULT_FUNCTIONAPP --storage-account $DEFAULT_STORAGE_ACCOUNT --query "state" -o tsv
-az functionapp config appsettings set --name $DEFAULT_FUNCTIONAPP -g $DEFAULT_RESOURCE_GROUP --settings SUBSCRIPTION=$SUBSCRIPTION_RESOURCE_ID DEFAULT_EXPIRY_DAYS=14 > /dev/null
-echo "Configuring easy auth for functionapp $DEFAULT_FUNCTIONAPP"
-az webapp auth update --ids $DEFAULT_RESOURCE_GROUP_RESOURCE_ID/providers/Microsoft.Web/sites/$DEFAULT_FUNCTIONAPP --action LoginWithAzureActiveDirectory --enabled true --aad-client-id $DEFAULT_APPLICATION_ID --aad-client-secret "$DEFAULT_APPLICATION_PASSWORD" --aad-token-issuer-url https://login.microsoftonline.com/$TENANTID/  > /dev/null
-echo "Configuring identity for functionapp $DEFAULT_FUNCTIONAPP with contributor access to scope $SUBSCRIPTIONID. Principal id:"
-az functionapp identity assign --name $DEFAULT_FUNCTIONAPP --resource-group $DEFAULT_RESOURCE_GROUP --role contributor --scope "$SUBSCRIPTIONID" --query principalId -o tsv
+az functionapp config appsettings set --name $DEFAULT_FUNCTIONAPP -g $DEFAULT_RESOURCE_GROUP --settings SUBSCRIPTION=$SUBSCRIPTIONID DEFAULT_EXPIRY_DAYS=14 DEFAULT_TAG=experiment > /dev/null
+echo "Configuring identity for functionapp $DEFAULT_FUNCTIONAPP with contributor access to scope $SUBSCRIPTION_RESOURCE_ID. Principal id:"
+DEFAULT_FUNCTIONAPP_PRINCIPALID=`az functionapp identity assign --name $DEFAULT_FUNCTIONAPP --resource-group $DEFAULT_RESOURCE_GROUP --role contributor --scope "$SUBSCRIPTION_RESOURCE_ID" --query principalId -o tsv`
 echo "Deploying function app $DEFAULT_FUNCTIONAPP"
 func azure functionapp publish $DEFAULT_FUNCTIONAPP
 
 echo "Generating cleanup script"
 echo "#!/bin/bash
-echo 'Removing app registration $DEFAULT_APPLICATION_IDENTIFIER_URI'
-az ad app delete --id $DEFAULT_APPLICATION_IDENTIFIER_URI
 echo 'Removing resource group $DEFAULT_RESOURCE_GROUP'
 az group delete --name $DEFAULT_RESOURCE_GROUP --yes
-
-
+echo 'Removing function app identity $DEFAULT_FUNCTIONAPP_PRINCIPALID'
+    az ad app delete --id $DEFAULT_FUNCTIONAPP_PRINCIPALID
 rm env.sh
 " > cleanup.sh
 chmod +x cleanup.sh
         
-echo "                 App id: $DEFAULT_APPLICATION_ID"
-echo "                App URI: $DEFAULT_APPLICATION_IDENTIFIER_URI"
-echo "           App password: $DEFAULT_APPLICATION_PASSWORD"
 echo "    Resource group name: $DEFAULT_RESOURCE_GROUP"
 echo "  Storage account (def): $DEFAULT_STORAGE_ACCOUNT"
 echo "      Storage key (def): $DEFAULT_STORAGE_ACCOUNT_CONNECTION_STRING"
